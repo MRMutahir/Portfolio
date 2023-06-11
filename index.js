aea4cb418e3b0705f013a12d61475b564c9c3aa8
Mhummad Mutahir <mutahirkareem820@gmail.com>
2023-06-12 03:06:33 +0500

    makemistake

---
+++ index.js
@@ -0,0 +1,39 @@
+let modalBody = document.querySelector('.m-b');
+let modal_video = document.querySelector('.modal_video');
+let description = document.querySelector('.description');
+let web_links = document.querySelector('.web_links');
+let web_1 = document.querySelector('.web_1');
+let web_2 = document.querySelector('.web_2');
+let web_3 = document.querySelector('#web_3');
+// console.log(web_3)
+// console.log(modalBody, modal_video, description, web_links);
+// console.log(web_1, web_2, web_3)
+// display(web_1)
+
+let obj1 = [{
+    id: 1,
+    Description: 'BISMILALLAH',
+    videoSrc: 'Bandicam/bandicam 2023-06-12 01-45-27-003.mp4',
+    link: 'https://taupe-profiterole-7eea16.netlify.app',
+}, {
+    id: 2,
+    Description: 'BISMILALLAH',
+    videoSrc: 'c:\Users\PC\Documents\Bandicam\bandicam 2023-06-12 02-02-58-114.mp4',
+    link: 'https://unrivaled-cucurucho-e5e8e9.netlify.app',
+}, {
+    id: 3,
+    Description: 'BISMILALLAH',
+    videoSrc: 'c:\Users\PC\Documents\Bandicam\bandicam 2023-06-12 02-06-50-896.mp4',
+    link: 'https://resilient-granita-b816a3.netlify.app'
+}];
+
+function display() {
+    // console.log(obj1[2])
+    // console.log(a + "tahir")
+    // console.log(a)
+}
+
+// let a = display()
+// web_1.addEventListener('click', display)
+// web_2.addEventListener('click', display)
+// web_3.addEventListener('click', display)
